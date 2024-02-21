7720213de90f9686e6efd6e616accb7ff2d50c7c
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-02-21 20:37:44 +0100

    added new stuff

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 
+/* Hover-effekt + rendering av namn på planterna */
 import { galaxy, handleMouseOver } from '/js/mouseover.js';
+
+/* API */
 import { fetchPlanetInfo, planetClickListener } from '/js/api.js';

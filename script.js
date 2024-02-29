caa57f70c1191c754d47ba2be2566db7fc82ab4e
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-02-29 15:12:52 +0100

    small changes

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 import { handleMouseOver } from '/js/mouseover.js';
 import { handlePlanetDisplay, displayElement } from '/js/api.js';
 
-
 // Initial display of planet information (for "jorden")
 handlePlanetDisplay("jorden", displayElement);
 handleMouseOver("jorden", displayElement);
